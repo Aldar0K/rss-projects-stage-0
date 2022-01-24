5412fec7ac8fc6f27ed47d3d4ceb99f47718280b
Aldar <aldarkonov@mail.ru>
2022-01-24 16:54:27 +0300

    feat: add menu burger

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,1 +1,13 @@
+// Выводим в консль самооценку по заданию
 console.log('Score: 110 / 110');
+
+// Меню бургер
+const iconMenu = document.querySelector('.menu__icon');
+if (iconMenu) {
+    const menuBody = document.querySelector('.menu__body');
+    iconMenu.addEventListener("click", function () {
+        document.body.classList.toggle('.body_lock')
+        iconMenu.classList.toggle('menu__icon_active');
+        menuBody.classList.toggle('menu__body_active');
+    })
+}
