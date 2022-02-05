d2a852648a522ef513c83c4a831e370c459a5a1b
Aldar <aldarkonov@mail.ru>
2022-02-05 14:28:21 +0300

    feat: finish task

--- eco-sounds/index.js
+++ eco-sounds/index.js
@@ -13,7 +13,33 @@ function preloadImages() {
 
 preloadImages()
 
-// Переключение кнопок  и картинок.
+// Введение переменной для тега audio.
+const audio = document.querySelector('audio');
+
+// Флаг, отображающий, играет музыка в данный момент или нет. По умолчанию - false.
+let isPlay = false;
+
+// Стоп или пауза трека и переключение кнопки play.
+const playBtn = document.querySelector('.play');
+
+function switchAudio () {
+    if (!isPlay) {
+        audio.currentTime = 0;
+        audio.play();
+
+        playBtn.classList.add('pause');
+        isPlay = true;
+    } else {
+        audio.pause();
+
+        playBtn.classList.remove('pause');
+        isPlay = false;
+    }
+}
+
+playBtn.addEventListener('click', switchAudio);
+
+// Переключение кнопок, картинок и треков.
 const navItem = document.querySelectorAll('.nav__item');
 const mainImage = document.querySelector('.main__image');
 const logo = document.querySelector('.header__logo');
@@ -21,18 +47,31 @@ const headerIcon = document.querySelector('.header__icon');
 
 function changeItemActive (event) {
     navItem.forEach(item => item.classList.remove('active'));
-    event.target.classList.add('active');
-
     headerIcon.classList.remove('icon_active');
 
+    event.target.classList.add('active');
+
     mainImage.src = `./assets/img/jpg/${event.target.dataset.bird}.jpg`;
+    audio.src = `./assets/audio/${event.target.dataset.bird}.mp3`
+
+    audio.currentTime = 0;
+    audio.play();
+    playBtn.classList.add('pause');
+    isPlay = true;
 }
 
 navItem.forEach(item => item.addEventListener('click', changeItemActive));
 
 logo.addEventListener('click', () => {
-    mainImage.src = `./assets/img/jpg/default.jpg`;
-
     navItem.forEach(item => item.classList.remove('active'));
+
     headerIcon.classList.add('icon_active');
+
+    mainImage.src = `./assets/img/jpg/default.jpg`;
+    audio.src = `./assets/audio/forest.mp3`
+
+    audio.currentTime = 0;
+    audio.play();
+    playBtn.classList.add('pause');
+    isPlay = true;
 });
