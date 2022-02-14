a9e0a833a41f5aa822ef1744caedf21668fa2cb8
Aldar <aldarkonov@mail.ru>
2022-02-14 17:19:10 +0300

    feat: finish task random-jokes

--- random-jokes/index.js
+++ random-jokes/index.js
@@ -1,13 +1,18 @@
+// Функция, генерирующая рандомное целое число в диапазоне.
 function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min; // Максимум и минимум включаются.
 }
 
+
+// Ввод переменных.
 const mainContent = document.querySelector('.main__content');
 const mainButton = document.querySelector('.main__button');
 const audio = document.querySelector('audio');
 
+
+// Ассинхронная функция, берущая рандомную цитату из файла data.json
 async function getQuotes() {  
     const quotes = 'data.json';
     const res = await fetch(quotes);
@@ -19,10 +24,13 @@ async function getQuotes() {
 
 getQuotes();
 
+
+// Функция для проигрывания звука.
 function playSound() {
     audio.play();
-    console.log('sound!');
 }
 
+
+// Добавляем кнопке слушатели.
 mainButton.addEventListener('click', getQuotes);
 mainButton.addEventListener('click', playSound);
