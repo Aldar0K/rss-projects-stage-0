84b5e954c7080522744357c878036f8d11949f2b
Aldar <aldarkonov@mail.ru>
2022-02-14 17:06:40 +0300

    feat: add switch sound for button

--- random-jokes/index.js
+++ random-jokes/index.js
@@ -1,8 +1,28 @@
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min; // Максимум и минимум включаются.
+}
+
+const mainContent = document.querySelector('.main__content');
+const mainButton = document.querySelector('.main__button');
+const audio = document.querySelector('audio');
+
 async function getQuotes() {  
     const quotes = 'data.json';
     const res = await fetch(quotes);
     const data = await res.json(); 
-    console.log(data);
+    // console.log(data[getRandomIntInclusive(0, 99)].joke);
+
+    mainContent.textContent = data[getRandomIntInclusive(0, 99)].joke;
 }
 
 getQuotes();
+
+function playSound() {
+    audio.play();
+    console.log('sound!');
+}
+
+mainButton.addEventListener('click', getQuotes);
+mainButton.addEventListener('click', playSound);
