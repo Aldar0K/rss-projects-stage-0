e94c51092835b67d701c795c19743f8f4676af22
Aldar <aldarkonov@mail.ru>
2022-02-20 19:02:51 +0300

    feat: finish task

--- tic-tac-toe/index.js
+++ tic-tac-toe/index.js
@@ -5,7 +5,6 @@ console.log('Score: 60 / 60');
 const area = document.getElementById('area');
 let move = 0;
 let result = '';
-// let winsStats = '';
 const contentWrapper = document.getElementById('content');
 const modalResult = document.getElementById('modal-result-wrapper');
 const overlay = document.getElementById('overlay');
@@ -118,87 +117,68 @@ function reloadPage () {
     location.reload();
 }
 
-
 // Перезагрузка страницы при нажатии на btn-reset, btn-close или вне всплывающего окна.
 overlay.addEventListener('click', reloadPage);
 btnClose.addEventListener('click', reloadPage);
 btnReset.addEventListener('click', reloadPage);
 
 // Таблица результатов последних 10 партий.
-
-// statsItem1.innerHTML = `1. ${localStorage.getItem('gameStats').slice(0, 1)} Win`;
-// statsItem2.innerHTML = `2. ${localStorage.getItem('gameStats').slice(1, 2)} Win`;
-// statsItem3.innerHTML = `3. ${localStorage.getItem('gameStats').slice(2, 3)} Win`;
-// statsItem4.innerHTML = `4. ${localStorage.getItem('gameStats').slice(3, 4)} Win`;
-// statsItem5.innerHTML = `5. ${localStorage.getItem('gameStats').slice(4, 5)} Win`;
-// statsItem6.innerHTML = `6. ${localStorage.getItem('gameStats').slice(5, 6)} Win`;
-// statsItem7.innerHTML = `7. ${localStorage.getItem('gameStats').slice(6, 7)} Win`;
-// statsItem8.innerHTML = `8. ${localStorage.getItem('gameStats').slice(7, 8)} Win`;
-// statsItem9.innerHTML = `9. ${localStorage.getItem('gameStats').slice(8, 9)} Win`;
-// statsItem10.innerHTML = `10. ${localStorage.getItem('gameStats').slice(9)} Win`;
-
 if (localStorage.getItem('gameStats').slice(0, 1) === 'T') {
     statsItem1.innerHTML = `1. Tie`;
 } else {
-    statsItem1.innerHTML = `1. "${localStorage.getItem('gameStats').slice(0, 1)}" Win`;
+    statsItem1.innerHTML = `1. "${localStorage.getItem('gameStats').slice(0, 1)}" win`;
 }
 
 if (localStorage.getItem('gameStats').slice(1, 2) === 'T') {
     statsItem2.innerHTML = `2. Tie`;
 } else {
-    statsItem2.innerHTML = `2. "${localStorage.getItem('gameStats').slice(1, 2)}" Win`;
+    statsItem2.innerHTML = `2. "${localStorage.getItem('gameStats').slice(1, 2)}" win`;
 }
 
 if (localStorage.getItem('gameStats').slice(2, 3) === 'T') {
     statsItem3.innerHTML = `3. Tie`;
 } else {
-    statsItem3.innerHTML = `3. "${localStorage.getItem('gameStats').slice(2, 3)}" Win`;
+    statsItem3.innerHTML = `3. "${localStorage.getItem('gameStats').slice(2, 3)}" win`;
 }
 
 if (localStorage.getItem('gameStats').slice(3, 4) === 'T') {
     statsItem4.innerHTML = `4. Tie`;
 } else {
-    statsItem4.innerHTML = `4. "${localStorage.getItem('gameStats').slice(3, 4)}" Win`;
+    statsItem4.innerHTML = `4. "${localStorage.getItem('gameStats').slice(3, 4)}" win`;
 }
 
 if (localStorage.getItem('gameStats').slice(4, 5) === 'T') {
     statsItem5.innerHTML = `5. Tie`;
 } else {
-    statsItem5.innerHTML = `5. "${localStorage.getItem('gameStats').slice(4, 5)}" Win`;
+    statsItem5.innerHTML = `5. "${localStorage.getItem('gameStats').slice(4, 5)}" win`;
 }
 
 if (localStorage.getItem('gameStats').slice(5, 6) === 'T') {
     statsItem6.innerHTML = `6. Tie`;
 } else {
-    statsItem6.innerHTML = `6. "${localStorage.getItem('gameStats').slice(5, 6)}" Win`;
+    statsItem6.innerHTML = `6. "${localStorage.getItem('gameStats').slice(5, 6)}" win`;
 }
 
 if (localStorage.getItem('gameStats').slice(6, 7) === 'T') {
     statsItem7.innerHTML = `7. Tie`;
 } else {
-    statsItem7.innerHTML = `7. "${localStorage.getItem('gameStats').slice(6, 7)}" Win`;
+    statsItem7.innerHTML = `7. "${localStorage.getItem('gameStats').slice(6, 7)}" win`;
 }
 
 if (localStorage.getItem('gameStats').slice(7, 8) === 'T') {
     statsItem8.innerHTML = `8. Tie`;
 } else {
-    statsItem8.innerHTML = `8. "${localStorage.getItem('gameStats').slice(7, 8)}" Win`;
+    statsItem8.innerHTML = `8. "${localStorage.getItem('gameStats').slice(7, 8)}" win`;
 }
 
 if (localStorage.getItem('gameStats').slice(8, 9) === 'T') {
     statsItem9.innerHTML = `9. Tie`;
 } else {
-    statsItem9.innerHTML = `9. "${localStorage.getItem('gameStats').slice(8, 9)}" Win`;
+    statsItem9.innerHTML = `9. "${localStorage.getItem('gameStats').slice(8, 9)}" win`;
 }
 
 if (localStorage.getItem('gameStats').slice(9) === 'T') {
     statsItem10.innerHTML = `10. Tie`;
 } else {
-    statsItem10.innerHTML = `10. "${localStorage.getItem('gameStats').slice(9)}" Win`;
+    statsItem10.innerHTML = `10. "${localStorage.getItem('gameStats').slice(9)}" win`;
 }
-
-function localStoragesInfo() {
-    console.log(localStorage.getItem('gameStats').length);
-    console.log(localStorage.getItem('gameStats'));
-}
-window.addEventListener('load', localStoragesInfo);
