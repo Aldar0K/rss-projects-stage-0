831e7e9414113193705097ce465fdeba0fbcb1ad
Aldar <aldarkonov@mail.ru>
2022-02-21 13:25:31 +0300

    feat: add clear button in stats

--- tic-tac-toe/index.js
+++ tic-tac-toe/index.js
@@ -10,6 +10,7 @@ const modalResult = document.getElementById('modal-result-wrapper');
 const overlay = document.getElementById('overlay');
 const btnClose = document.getElementById('btn-close');
 const btnReset = document.getElementById('btn-reset');
+const btnClear = document.getElementById('btn-clear');
 const turnSound = document.querySelector('.turn-sound');
 const statsItem1 = document.getElementById('stats__item1');
 const statsItem2 = document.getElementById('stats__item2');
@@ -43,9 +44,7 @@ function playTurnSound() {
 
 // Функция проверки.
 function check () {
-    // const boxes = document.querySelectorAll('.box');
     const boxes = document.getElementsByClassName('box');
-    // console.log(boxes);
 
     const winsArr = [
         [0, 1, 2],
@@ -65,10 +64,11 @@ function check () {
             result = 'X';
             prepareResult(result);
             setWinStats(result);
+            console.log('"X" won!');
             break;
-        }
+        } 
 
-        else if (
+        if (
             boxes[winsArr[i][0]].innerHTML == '0' && boxes[winsArr[i][1]].innerHTML == '0' && boxes[winsArr[i][2]].innerHTML == '0'
         ) {
             result = '0';
@@ -77,27 +77,14 @@ function check () {
             break;
         }
 
-        if (move === 9) {
+        else if (move === 9) {
             result = 'T';
             prepareResult(result);
             setWinStats(result);
+            console.log('Tie!');
             break;
         }
     }
-}
-
-// Функция записи результатов последних 10 партий.
-function setWinStats (winner) {
-    let winsStats = localStorage.getItem('gameStats');
-
-    if (winsStats.length < 10) {
-        winsStats = winner + winsStats;
-    } else {
-        winsStats = winsStats.slice(0, 9);
-        winsStats = winner + winsStats;
-    }
-
-    localStorage.setItem('gameStats', winsStats);
 }
 
 // Функция подготовки результата.
@@ -108,6 +95,26 @@ function prepareResult (winner) {
     } else {
         contentWrapper.innerHTML = `Player "${winner}" won!\nTurns: ${move}`;
         modalResult.style.display = 'block';
+    }
+}
+
+// Функция записи результатов последних 10 партий.
+function setWinStats (winner) {
+    if (localStorage.getItem('gameStats') !== null) {
+        let winsStats = localStorage.getItem('gameStats');
+
+        if (winsStats.length < 10) {
+            winsStats = winner + winsStats;
+        } else {
+            winsStats = winsStats.slice(0, 9);
+            winsStats = winner + winsStats;
+        }
+
+        localStorage.setItem('gameStats', winsStats);
+    } else {
+        winsStats = winner;
+
+        localStorage.setItem('gameStats', winsStats);
     }
 }
 
@@ -123,62 +130,74 @@ btnClose.addEventListener('click', reloadPage);
 btnReset.addEventListener('click', reloadPage);
 
 // Таблица результатов последних 10 партий.
-if (localStorage.getItem('gameStats').slice(0, 1) === 'T') {
-    statsItem1.innerHTML = `1. Tie`;
-} else {
-    statsItem1.innerHTML = `1. "${localStorage.getItem('gameStats').slice(0, 1)}" win`;
+if (localStorage.getItem('gameStats') !== null) {
+    if (localStorage.getItem('gameStats').slice(0, 1) === 'T') {
+        statsItem1.innerHTML = `1. Tie`;
+    } else if (localStorage.getItem('gameStats').slice(0, 1) === 'X' || localStorage.getItem('gameStats').slice(0, 1) === '0') {
+        statsItem1.innerHTML = `1. "${localStorage.getItem('gameStats').slice(0, 1)}" win`;
+    }
+    
+    if (localStorage.getItem('gameStats').slice(1, 2) === 'T') {
+        statsItem2.innerHTML = `2. Tie`;
+    } else if (localStorage.getItem('gameStats').slice(1, 2) === 'X' || localStorage.getItem('gameStats').slice(1, 2) === '0') {
+        statsItem2.innerHTML = `2. "${localStorage.getItem('gameStats').slice(1, 2)}" win`;
+    }
+    
+    if (localStorage.getItem('gameStats').slice(2, 3) === 'T') {
+        statsItem3.innerHTML = `3. Tie`;
+    } else if (localStorage.getItem('gameStats').slice(2, 3) === 'X' || localStorage.getItem('gameStats').slice(2, 3) === '0') {
+        statsItem3.innerHTML = `3. "${localStorage.getItem('gameStats').slice(2, 3)}" win`;
+    }
+    
+    if (localStorage.getItem('gameStats').slice(3, 4) === 'T') {
+        statsItem4.innerHTML = `4. Tie`;
+    } else if (localStorage.getItem('gameStats').slice(3, 4) === 'X' || localStorage.getItem('gameStats').slice(3, 4) === '0') {
+        statsItem4.innerHTML = `4. "${localStorage.getItem('gameStats').slice(3, 4)}" win`;
+    }
+    
+    if (localStorage.getItem('gameStats').slice(4, 5) === 'T') {
+        statsItem5.innerHTML = `5. Tie`;
+    } else if (localStorage.getItem('gameStats').slice(4, 5) === 'X' || localStorage.getItem('gameStats').slice(4, 5) === '0') {
+        statsItem5.innerHTML = `5. "${localStorage.getItem('gameStats').slice(4, 5)}" win`;
+    }
+    
+    if (localStorage.getItem('gameStats').slice(5, 6) === 'T') {
+        statsItem6.innerHTML = `6. Tie`;
+    } else if (localStorage.getItem('gameStats').slice(5, 6) === 'X' || localStorage.getItem('gameStats').slice(5, 6) === '0') {
+        statsItem6.innerHTML = `6. "${localStorage.getItem('gameStats').slice(5, 6)}" win`;
+    }
+    
+    if (localStorage.getItem('gameStats').slice(6, 7) === 'T') {
+        statsItem7.innerHTML = `7. Tie`;
+    } else if (localStorage.getItem('gameStats').slice(6, 7) === 'X' || localStorage.getItem('gameStats').slice(6, 7) === '0') {
+        statsItem7.innerHTML = `7. "${localStorage.getItem('gameStats').slice(6, 7)}" win`;
+    }
+    
+    if (localStorage.getItem('gameStats').slice(7, 8) === 'T') {
+        statsItem8.innerHTML = `8. Tie`;
+    } else if (localStorage.getItem('gameStats').slice(7, 8) === 'X' || localStorage.getItem('gameStats').slice(7, 8) === '0') {
+        statsItem8.innerHTML = `8. "${localStorage.getItem('gameStats').slice(7, 8)}" win`;
+    }
+    
+    if (localStorage.getItem('gameStats').slice(8, 9) === 'T') {
+        statsItem9.innerHTML = `9. Tie`;
+    } else if (localStorage.getItem('gameStats').slice(8, 9) === 'X' || localStorage.getItem('gameStats').slice(8, 9) === '0') {
+        statsItem9.innerHTML = `9. "${localStorage.getItem('gameStats').slice(8, 9)}" win`;
+    }
+    
+    if (localStorage.getItem('gameStats').slice(9) === 'T') {
+        statsItem10.innerHTML = `10. Tie`;
+    } else if (localStorage.getItem('gameStats').slice(9) === 'X' || localStorage.getItem('gameStats').slice(9) === '0') {
+        statsItem10.innerHTML = `10. "${localStorage.getItem('gameStats').slice(9)}" win`;
+    }
 }
 
-if (localStorage.getItem('gameStats').slice(1, 2) === 'T') {
-    statsItem2.innerHTML = `2. Tie`;
-} else {
-    statsItem2.innerHTML = `2. "${localStorage.getItem('gameStats').slice(1, 2)}" win`;
+// Функция очистки таблицы результатов последних 10 партий.
+function statsClear () {
+    localStorage.clear();
+    reloadPage();
 }
 
-if (localStorage.getItem('gameStats').slice(2, 3) === 'T') {
-    statsItem3.innerHTML = `3. Tie`;
-} else {
-    statsItem3.innerHTML = `3. "${localStorage.getItem('gameStats').slice(2, 3)}" win`;
-}
+// Кнопка для очистки таблицы результатов последних 10 партий.
+btnClear.addEventListener('click', statsClear);
 
-if (localStorage.getItem('gameStats').slice(3, 4) === 'T') {
-    statsItem4.innerHTML = `4. Tie`;
-} else {
-    statsItem4.innerHTML = `4. "${localStorage.getItem('gameStats').slice(3, 4)}" win`;
-}
-
-if (localStorage.getItem('gameStats').slice(4, 5) === 'T') {
-    statsItem5.innerHTML = `5. Tie`;
-} else {
-    statsItem5.innerHTML = `5. "${localStorage.getItem('gameStats').slice(4, 5)}" win`;
-}
-
-if (localStorage.getItem('gameStats').slice(5, 6) === 'T') {
-    statsItem6.innerHTML = `6. Tie`;
-} else {
-    statsItem6.innerHTML = `6. "${localStorage.getItem('gameStats').slice(5, 6)}" win`;
-}
-
-if (localStorage.getItem('gameStats').slice(6, 7) === 'T') {
-    statsItem7.innerHTML = `7. Tie`;
-} else {
-    statsItem7.innerHTML = `7. "${localStorage.getItem('gameStats').slice(6, 7)}" win`;
-}
-
-if (localStorage.getItem('gameStats').slice(7, 8) === 'T') {
-    statsItem8.innerHTML = `8. Tie`;
-} else {
-    statsItem8.innerHTML = `8. "${localStorage.getItem('gameStats').slice(7, 8)}" win`;
-}
-
-if (localStorage.getItem('gameStats').slice(8, 9) === 'T') {
-    statsItem9.innerHTML = `9. Tie`;
-} else {
-    statsItem9.innerHTML = `9. "${localStorage.getItem('gameStats').slice(8, 9)}" win`;
-}
-
-if (localStorage.getItem('gameStats').slice(9) === 'T') {
-    statsItem10.innerHTML = `10. Tie`;
-} else {
-    statsItem10.innerHTML = `10. "${localStorage.getItem('gameStats').slice(9)}" win`;
-}
